99067b36cf328f3b4613552c9da6ccbafb750ffc
Hishaam <hishaamtchirou73@gmail.com>
2023-08-22 17:03:32 +0000

    feat: movies

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,10 +1,13 @@
 import { signOut } from 'next-auth/react'
 
+import useCurrentUser from '@/hooks/useCurrentUser'
+
 interface AccountMenuProps {
     visible?: boolean
 }
 
 const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
+    const {data: user} = useCurrentUser()
     if (!visible) {
         return null
     }
@@ -19,7 +22,7 @@ const AccountMenu: React.FC<AccountMenuProps> = ({ visible }) => {
                         className="w-8 rounded-md"
                     />
                     <p className="text-white text-sm grooup-hover/item:underline">
-                        Username
+                        {user.name}
                     </p>
                 </div>
                 <hr className="bg-gray-600 border-0 h-px my-4" />
